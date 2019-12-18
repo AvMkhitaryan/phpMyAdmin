5f14272e668d683351659862c12a4f2f27bfa0f1
Avetik Mkhitaryan <av_mkhitaryan@bk.ru>
2019-12-18 17:09:21 +0400

    click open table and info for object

--- js/index.js
+++ js/index.js
@@ -207,220 +207,222 @@ function reversFun(arg) {
                                 </div>
                             </div>  `;
     } else {
-        content.innerHTML = "db";
-    }
-}
+        content.innerHTML = `<div class="filse">
+                                <fieldset id="fld">
+                                    <legend id="leg">Фильтры</legend>
+                                    <div class="d-flex">
+                                        <h6>Содержит слово:</h6>
+                                        <input type="text" id="fld-input">
+                                    </div>
+                                </fieldset>
+                            </div>
+                            <div class="filse">
+                                <table class="tab">
+                                    <tr>
+                                        <th>Таблица</th>
+                                        <th>Действие</th>
+                                        <th>Строки</th>
+                                        <th>Тип</th>
+                                        <th>Сравнение</th>
+                                    </tr>
+<!--                                    <tr>-->
+<!--                                        <td class="tr-first-td"><input type="checkbox" name="vehicle" value="db">sdsdghfghfghgfhfghfghs</td>-->
+<!--                                        <td>-->
+<!--                                            <img src="img/b_no_favorite.png" alt="star">-->
+<!--                                            <img src="img/bd_browse.png" alt="overview">-->
+<!--                                            <a href="#">Обзор</a>-->
+<!--                                            <img src="img/b_props.png" alt="Structure">-->
+<!--                                            <a href="#">Структура</a>-->
+<!--                                            <img src="img/b_select.png" alt="search">-->
+<!--                                            <a href="">Поиск</a>-->
+<!--                                        </td>-->
+<!--                                        <td>-->
+<!--                                            <a href="#">~0</a>-->
+<!--                                            <img src="img/b_docs.png" alt="what?">-->
+<!--                                        </td>-->
+<!--                                        <td>Aria</td>-->
+<!--                                        <td>utf8_general_ci</td>-->
+<!--                                    </tr>-->
+                                </table>
+                            </div> `;
 
+        let tableDb = document.getElementsByClassName("tab");
 
-let myObjects = [
-    {
-        name: 'Базы данных',
-        src: 'img/s_db.png'
-    },
-    {
-        name: 'Sql',
-        src: 'img/b_sql.png'
-    },
-    {
-        name: 'Состояние',
-        src: 'img/s_status.png'
-    },
-    {
-        name: 'Учетные записи пользователей',
-        src: 'img/s_rights.png'
-    },
-    {
-        name: 'Экспорт',
-        src: 'img/b_export.png'
-    },
-    {
-        name: 'Импорт',
-        src: 'img/b_import.png'
-    },
-    {
-        name: 'Настройки',
-        src: 'img/b_tblops.png'
-    },
-    {
-        name: 'Репликация',
-        src: 'img/s_replication.png'
-    },
-    {
-        name: 'Переменные',
-        src: 'img/s_link.png'
-    },
-    {
-        name: 'Кодировки',
-        src: 'img/s_asci.png'
-    },
-    {
-        name: 'Типы таблиц',
-        src: 'img/b_engine.png'
-    },
-    {
-        name: 'Расширения',
-        src: 'img/b_plugin.png'
-    },
+        for (let h in dbInfoObj) {
+            let trone = document.createElement("tr");
+            tableDb.appendChild(trone);
 
+            let troneTdone=document.createElement("td");
+            troneTdone.setAttribute("class","tr-first-td");
+            trone.appendChild(troneTdone);
 
-];
+            let chekInput = document.createElement("input");
+            chekInput.setAttribute("type","checkbox");
+            chekInput.setAttribute("name","vehicle");
+            chekInput.setAttribute("value","db");
+            troneTdone.innerHTML=dbInfoObj[h].tab;
+            troneTdone.appendChild(chekInput);
 
-let idNav = document.getElementById('nb-id');
+            let tdtwo=document.createElement("td");
+            trone.appendChild(tdtwo);
 
-for (let i in myObjects) {
+            let imgone =document.createElement("img");
+            imgone.setAttribute("src","img/b_no_favorite.png");
+            imgone.setAttribute("alt","star");
+            tdtwo.appendChild(imgone);
 
-    let a = document.createElement('a');
-    a.setAttribute('class', 'nav-a-style');
-    a.setAttribute('href', '#');
-    idNav.appendChild(a);
+            let imgtwo =document.createElement("img");
+            imgtwo.setAttribute("src","img/bd_browse.png");
+            imgtwo.setAttribute("alt","overview");
+            tdtwo.appendChild(imgtwo);
 
-    let img = document.createElement('img');
-    img.setAttribute('src', myObjects[i].src);
-    img.setAttribute("alt", "logo");
-    a.appendChild(img);
+            let tedtwoA =document.createElement("a");
+            tedtwoA.setAttribute("href","#");
+            tedtwoA.innerHTML="Обзор";
+            tdtwo.appendChild(tedtwoA);
 
-    a.innerHTML += myObjects[i].name;
-}
+            let imgthree =document.createElement("img");
+            imgthree.setAttribute("src","img/b_props.png");
+            imgthree.setAttribute("alt","Structure");
+            tdtwo.appendChild(imgthree);
 
-let collObj = [
-    {
-        nameone: "databasa",
-        title: [
-            {n: "Dtabasa1-1"},
-            {n: "Dtabasa1-2"},
+            let tedtwoAtwo =document.createElement("a");
+            tedtwoAtwo.setAttribute("href","#");
+            tedtwoAtwo.innerHTML="Структура";
+            tdtwo.appendChild(tedtwoAtwo);
 
-        ]
-    },
-    {
-        nameone: "databasa11",
-        title: [
-            {n: "Dtabasa2-1"},
-            {n: "Dtabasa2-2"},
+            let imgfore =document.createElement("img");
+            imgfore.setAttribute("src","img/b_select.png");
+            imgfore.setAttribute("alt","search");
+            tdtwo.appendChild(imgfore);
 
-        ]
-    },
+            let aSerch =document.createElement("a");
+            aSerch.setAttribute("href","#");
+            aSerch.innerHTML="Структура";
+            tdtwo.appendChild(aSerch);
 
-];
-
-let cdiv = document.getElementById("collapse-divs");
-
-for (let as in collObj) {
-
-    let div = document.createElement("div");
-    div.setAttribute("class", "db-divs");
-    cdiv.appendChild(div);
-
-    let divone = document.createElement("div");
-    div.appendChild(divone);
-
-    let a = document.createElement("a");
-    a.setAttribute("href", "#demo" + as);
-    a.setAttribute("class", "btn btn-link");
-    a.setAttribute("data-toggle", "collapse");
-    divone.appendChild(a);
-
-    let i = document.createElement("i");
-    i.setAttribute("class", "fa fa-plus");
-    i.setAttribute("aria-hidden", "true");
-    a.appendChild(i);
-
-    let button = document.createElement("button");
-    button.setAttribute("class", "btn btn-link");
-    button.setAttribute("onclick", "reversFun(" + (+as + 2) + ")");
-    button.innerHTML = collObj[as].nameone;
-    divone.appendChild(button);
-
-    let divonea = document.createElement("div");
-    divonea.setAttribute("id", "demo" + as);
-    divonea.setAttribute("class", "collapse first-demo");
-    cdiv.appendChild(divonea);
-
-
-    for (let vs in collObj[as].title) {
-
-            console.log(vs);
-        let divA = document.createElement("a");
-        divA.setAttribute("href", "#demo" + (+vs+(+as)));
-        divA.setAttribute("class", "btn btn-link last-toggle");
-        divA.setAttribute("data-toggle", "collapse");
-        divonea.appendChild(divA);
-
-        let iconsOne = document.createElement("i");
-        iconsOne.setAttribute("class", "fa fa-plus");
-        iconsOne.setAttribute("aria-hidden", "true");
-        iconsOne.innerHTML = collObj[as].title[vs].n;
-        divA.appendChild(iconsOne);
-
-        let divtwo = document.createElement("div");
-        divtwo.setAttribute("id", "demo" + + (+vs+(+as)));
-        divtwo.setAttribute("class", "collapse demo-info");
-        divonea.appendChild(divtwo);
-
-        let paragraf = document.createElement("p");
-        paragraf.innerHTML = "text";
-        divtwo.appendChild(paragraf);
+        }
 
     }
 }
 
-// for (let as in collObj) {
-//
-//     //<div class="db-divs">
-//     let colldiv = document.createElement("div");
-//     colldiv.setAttribute("class", "db-divs");
-//     cdiv.appendChild(colldiv);
-//
-//     //div
-//     let colldivone = document.createElement("div");
-//     colldiv.appendChild(colldivone);
-//
-//     let colldivoneAAA = document.createElement("a");
-//     colldivoneAAA.setAttribute("href", "#demo" + (+as));
-//     colldivoneAAA.setAttribute("class", "btn btn-link");
-//     colldivoneAAA.setAttribute("data-toggle", "collapse");
-//     colldivone.appendChild(colldivoneAAA);
-//
-//     let iconfirst = document.createElement("i");
-//     iconfirst.setAttribute("class", "fa fa-plus");
-//     iconfirst.setAttribute("aria-hidden", "true");
-//     colldivoneAAA.appendChild(iconfirst);
-//
-//     let butone = document.createElement("button");
-//     butone.setAttribute("class", "btn btn-link");
-//     butone.setAttribute("onclick", "reversFun(" + (+as+2) + ")");
-//     butone.innerHTML = collObj[as].nameone;
-//     colldivoneAAA.appendChild(butone);
-//
-//     let infodiv = document.createElement("div");
-//     infodiv.setAttribute("id", "demo");
-//     infodiv.setAttribute("class", "collapse first-demo");
-//     colldiv.appendChild(infodiv);
-//
-//     for (let vs in collObj[as].title) {
-//
-//         let divA = document.createElement("a");
-//         divA.setAttribute("href", "#demo" + (+vs));
-//         divA.setAttribute("class", "btn btn-link last-toggle");
-//         divA.setAttribute("data-toggle", "collapse");
-//         infodiv.appendChild(divA);
-//
-//         let iconsOne = document.createElement("i");
-//         iconsOne.setAttribute("class", "fa fa-plus");
-//         iconsOne.setAttribute("aria-hidden", "true");
-//         iconsOne.innerHTML=collObj[as].title[vs].n;
-//         divA.appendChild(iconsOne);
-//
-//         let divtwo=document.createElement("div");
-//         divtwo.setAttribute("id","demo"+ (+vs));
-//         divtwo.setAttribute("class","collapse demo-info");
-//         infodiv.appendChild(divtwo);
-//
-//         let paragraf=document.createElement("p");
-//         paragraf.innerHTML="text";
-//         divtwo.appendChild(paragraf);
-//
-//
-//     }
-//
-//
-// }
+let dbInfoObj = [
+    {
+        tab: "dt",
+        tipe: "arial",
+        unicode: "utf8_general_ci",
+    },
+    {
+        tab: "dtbb",
+        tipe: "arial1",
+        unicode: "utf16_general_ci",
+    }
+
+];
+
+
+$(document).ready(function () {
+    $.ajax({
+        url: 'json/index.json',
+        data: 'myObjects',
+        dataType: 'json',
+        success: function (data) {
+            let idNav = document.getElementById('nb-id');
+
+            for (let i in data.myObjects) {
+
+                let a = document.createElement('a');
+                a.setAttribute('class', 'nav-a-style');
+                a.setAttribute('href', '#');
+                idNav.appendChild(a);
+
+                let img = document.createElement('img');
+                img.setAttribute('src', data.myObjects[i].src);
+                img.setAttribute("alt", "logo");
+                a.appendChild(img);
+
+                a.innerHTML += data.myObjects[i].name;
+            }
+        }, error: function (error) {
+            console.log(error)
+        }
+    });
+
+    $.ajax({
+        url: 'json/index2.json',
+        data: 'collObj',
+        dataType: 'json',
+        success: function (data) {
+            let cdiv = document.getElementById("collapse-divs");
+
+            for (let as in data.collObj) {
+
+                let div = document.createElement("div");
+                div.setAttribute("class", "db-divs");
+                cdiv.appendChild(div);
+
+                let divone = document.createElement("div");
+                div.appendChild(divone);
+
+                let a = document.createElement("a");
+                a.setAttribute("href", "#demo" + as);
+                a.setAttribute("class", "btn btn-link");
+                a.setAttribute("data-toggle", "collapse");
+                divone.appendChild(a);
+
+                let i = document.createElement("i");
+                i.setAttribute("class", "fa fa-plus test");
+                i.setAttribute("aria-hidden", "true");
+                a.appendChild(i);
+
+                let button = document.createElement("button");
+                button.setAttribute("class", "btn btn-link");
+                button.setAttribute("onclick", "reversFun(" + (+as + 2) + ")");
+                button.innerHTML = data.collObj[as].nameone;
+                divone.appendChild(button);
+
+                let divonea = document.createElement("div");
+                divonea.setAttribute("id", "demo" + as);
+                divonea.setAttribute("class", "collapse first-demo flex-column");
+                cdiv.appendChild(divonea);
+
+
+                for (let vs in data.collObj[as].title) {
+
+                    let divA = document.createElement("a");
+                    divA.setAttribute("href", "#demo" + ((+vs + as + 2) ** 4));
+                    divA.setAttribute("class", "btn btn-link last-toggle");
+                    divA.setAttribute("data-toggle", "collapse");
+                    divonea.appendChild(divA);
+
+                    let iconsOne = document.createElement("i");
+                    iconsOne.setAttribute("class", "fa fa-plus test1");
+                    iconsOne.setAttribute("aria-hidden", "true");
+                    iconsOne.innerHTML = data.collObj[as].title[vs].n;
+                    divA.appendChild(iconsOne);
+
+                    let divtwo = document.createElement("div");
+                    divtwo.setAttribute("id", "demo" + ((+vs + as + 2) ** 4));
+                    divtwo.setAttribute("class", "collapse demo-info");
+                    divonea.appendChild(divtwo);
+
+                    let paragraf = document.createElement("p");
+                    paragraf.innerHTML = "text";
+                    divtwo.appendChild(paragraf);
+
+                }
+            }
+        }, error: function (error) {
+            console.log(error)
+        }
+    })
+});
+
+
+$(document).on('click', '.test', function () {
+    $(this).toggleClass('fa-minus');
+
+});
+$(document).on('click', '.test1', function () {
+    $(this).toggleClass('fa-minus');
+});
